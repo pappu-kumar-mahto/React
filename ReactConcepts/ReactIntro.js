bb5bb519a1968cde6b3f16ece18b3022d334bc7a
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-05-31 00:22:14 +0530

    React started: how it works, virtual DOM, SPA, CDN are discussed. NodeJS and npm installation done.

---
+++ ReactConcepts/ReactIntro.js
@@ -0,0 +1,6 @@
+/* React
+	- All about components
+	- What to do! Not how to do!
+	- SPA - Single Page Application (Single html page, rest everything controlled by React.js library)
+	- Uses Virtual DOM
+*/
