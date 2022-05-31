22d17cb5a76c8b350b5124dc273a4f526ae7a6d5
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-06-01 00:16:02 +0530

    ReactJs: npm/npx, create-react-app, folder stracture.

--- ReactConcepts/ReactIntro.js
+++ ReactConcepts/ReactIntro.js
@@ -3,4 +3,8 @@
 	- What to do! Not how to do!
 	- SPA - Single Page Application (Single html page, rest everything controlled by React.js library)
 	- Uses Virtual DOM
+
+	- npm / npx
+	- create-react-app
+	- folder stracture
 */
