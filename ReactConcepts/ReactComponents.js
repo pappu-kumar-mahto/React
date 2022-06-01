7d5b1ae2261335e6792f9e2cbef112460a70505e
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-06-02 00:12:45 +0530

    ReactJs-1: Components: Types, features, JSX, JSON, XML, Fragments.

---
+++ ReactConcepts/ReactComponents.js
@@ -0,0 +1,27 @@
+/*
+Components -
+	Types:
+	1. Class Based Components
+	2. Function Based Components
+	- Components can be considered as sub-sections inside the entire application
+	- Returns JSX (JavaScript + XML)
+	- Can be Nested
+	- Can be Re-Used
+	Fragments - Empty JSX tags (Used for nesting multiple elements inside a single parent to be returned by the component)
+	/ ---------------------------------------------------------------- /
+	
+	JSON - JavaScript Object Notation
+	{
+		name: 'Vaibhav',
+		batch: 10
+	}
+	XML - Extensible Markup Language
+	<student>
+		<name>
+			Vaibhav
+		</name>
+		<batch>
+			10
+		</batch>
+	</student>
+*/
