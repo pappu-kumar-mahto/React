4df40e47814c857ce668af7eeb1ee3c12c2357b5
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-06-04 21:21:07 +0530

    [Project: myPortfolio]: Blog Post page started.

--- Projects/my-portfolio/src/components/Post.js
+++ Projects/my-portfolio/src/components/Post.js
@@ -1,5 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import sanityClient from '../client.js'
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-    return <h1>Post Page</h1>
+
+    const [postData, setPost] = useState(null)
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_type == "post]{
+            title,
+            slug,
+            mainImage{
+                asset->{
+                    _id,
+                    url
+                },
+                alt
+            }
+        }`)
+            .then((data) => setPost(data))
+            .catch(console.error)
+    }, [])
+
+    return (
+      <main>
+        <section>
+          <h1></h1>
+          <h2></h2>
+          <div>
+            <article>
+              <link>
+                <span>
+                  <img />
+                  <span>
+                    <h3></h3>
+                  </span>
+                </span>
+              </link>
+            </article>
+          </div>
+        </section>
+      </main>
+    );
 }
