29a3a79209ca2c364940eacf95a8d7597a93e110
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-06-04 16:24:57 +0530

    [Project: myPortfolio]: routing function working properly. with "Routes".

--- Projects/my-portfolio/src/App.js
+++ Projects/my-portfolio/src/App.js
@@ -1,7 +1,21 @@
-import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from "./components/Home"
+import About from "./components/About"
+import SinglePost from "./components/SinglePost"
+import Post from "./components/Post"
+import Project from "./components/Project"
 function App() {
-  return
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element = {<Home/>} path = "/" exact/>
+        <Route element = {<About/>} path = '/about' />
+        <Route element = {<SinglePost/>} path = '/post/:slug' />
+        <Route element = {<Post/>} path = '/post' />
+        <Route element = {<Project/>} path = '/project' />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
