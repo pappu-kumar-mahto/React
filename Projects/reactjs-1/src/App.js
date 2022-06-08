ce6ab17828175c099f35e7da262ee2a7ff3954b7
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-06-08 16:08:20 +0530

    ReactJs-1:  created some dummy components.

--- Projects/reactjs-1/src/App.js
+++ Projects/reactjs-1/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import ChildComponent1 from './components/ChildComponent1'
 import ChildComponent2 from './components/ChildComponent2'
+import ChildComponent3 from './components/ChildComponent3'
 
 function App() {
   return (
     <>
       <ChildComponent1/>
       <ChildComponent2/>
+      <ChildComponent3/>
     </>
   );
 }
---
+++ Projects/reactjs-1/src/components/ChildComponent3.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const ChildComponent3 = () =>{
+    return (
+        <div className="ChildComponent3">
+            <h1>
+                Child Component 3
+            </h1>
+        </div>
+    )
+}
+
+export default ChildComponent3
