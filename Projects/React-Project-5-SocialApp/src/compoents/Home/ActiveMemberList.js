1e2e2e0026efbb79568fe5740f25a93b2b20bc96
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-06-28 11:13:42 +0530

    React-Project-5: Social-App: Active and offline members list updated and some UI enhancements.

--- Projects/React-Project-5-SocialApp/src/compoents/Home/ActiveMemberList.js
+++ Projects/React-Project-5-SocialApp/src/compoents/Home/ActiveMemberList.js
@@ -5,23 +5,34 @@ const ActiveMemberList = ({ posts }) => {
   return (
     <Col md={3}>
       <ListGroup as="ol">
-        {posts.map((post) => (
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start active-member-list"
-            key={post.id}
-          >
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">
-                <TbUserCircle />
-                {post.author}
+        {posts.map((post) => {
+          let isMemberActive = Math.ceil(Math.random() * 2) % 2;
+          return (
+            <ListGroup.Item
+              as="li"
+              className={`d-flex justify-content-between align-items-start active-member-list ${
+                isMemberActive ? "inActive" : ""
+              }`}
+              key={post.id}
+            >
+              <div className="ms-2 me-auto">
+                <div className="fw-bold">
+                  <TbUserCircle />
+                  {post.author}
+                </div>
               </div>
-            </div>
-            <Badge bg="success" pill>
-              active
-            </Badge>
-          </ListGroup.Item>
-        ))}
+              {!isMemberActive ? (
+                <Badge bg="success" pill>
+                  active
+                </Badge>
+              ) : (
+                <Badge bg="secondary" pill>
+                  offline
+                </Badge>
+              )}
+            </ListGroup.Item>
+          );
+        })}
       </ListGroup>
     </Col>
   );
--- Projects/React-Project-5-SocialApp/src/compoents/Home/ChatList.js
+++ Projects/React-Project-5-SocialApp/src/compoents/Home/ChatList.js
@@ -1,31 +1,33 @@
 import { Col, ListGroup, Badge } from "react-bootstrap";
 import { TbUserCircle } from "react-icons/tb";
-import randomChatList from '../../assests/randomChatMsgs.json'
+import randomChatList from "../../assests/randomChatMsgs.json";
 import "./Home.css";
 const ChatList = ({ posts }) => {
-    return (
-        <Col md={3}>
-          <ListGroup as="ol">
-            {posts.map((post) => (
-              <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-start active-member-list"
-                key={post.id}
-              >
-                <div className="ms-2 me-auto">
-                  <div className="fw-bold">
-                    <TbUserCircle />
-                    {post.author}
-                        </div>
-                        {randomChatList[Math.ceil(Math.random()*7)].message}
-                </div>
-                <Badge bg="primary" pill>
-                  {randomChatList[Math.ceil(Math.random()*7)].messageCount}
-                </Badge>
-              </ListGroup.Item>
-            ))}
-          </ListGroup>
-        </Col>
-      );
-}
-export default ChatList
+  return (
+    <Col md={3}>
+      <ListGroup as="ol">
+        {posts.map((post) => (
+          <ListGroup.Item
+            as="li"
+            className="d-flex justify-content-between align-items-start active-member-list"
+            key={post.id}
+          >
+            <div className="ms-2 me-auto">
+              <div className="fw-bold" style={{textAlign:'left'}}>
+                <TbUserCircle />
+                {post.author}
+              </div>
+              <div style={{fontSize:'13px', textAlign:'left', opacity:'0.75'}}>
+              {randomChatList[Math.ceil(Math.random() * 7)].message}
+              </div>
+            </div>
+            <Badge bg="info" pill>
+              {randomChatList[Math.ceil(Math.random() * 7)].messageCount}
+            </Badge>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </Col>
+  );
+};
+export default ChatList;
