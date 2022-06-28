953b4badf28d371bd2110128119fa74d4449f2c2
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-06-28 20:28:10 +0530

    React-Project-6: NewsApp: Navbar, aboutUs, contactUs components added and linked using Routes.

--- Projects/React-Project-6-NewsApp/src/App.js
+++ Projects/React-Project-6-NewsApp/src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ Projects/React-Project-6-NewsApp/src/components/Contact.js
@@ -0,0 +1,11 @@
+import {  Container } from 'react-bootstrap'
+const Contact = () => {
+    return (
+        <Container>
+            <h1 className="display-2">Contact Us</h1>
+            <h3 className='display-4'>Email: socialAppOfficals@gmail.com</h3>
+            <h4 className='display-4'>Phone: +91-8899557744</h4>
+        </Container>
+    )
+}
+export default Contact
