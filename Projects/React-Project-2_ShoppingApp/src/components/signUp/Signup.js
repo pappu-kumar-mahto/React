952facb09010cf71ab00db32b9f9c10209d84451
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-07-23 21:40:47 +0530

    React-Project-2: Shopping App: SignUp complete with authentication.

--- Projects/React-Project-2_ShoppingApp/src/components/signUp/Signup.js
+++ Projects/React-Project-2_ShoppingApp/src/components/signUp/Signup.js
@@ -9,7 +9,13 @@ const LoopRender = () => {
     password: "",
     phoneNumber: "",
   });
-  // let [isError, setIsError]= useState(false)
+  let [isError, setIsError] = useState({
+    firstNameError: false,
+    lastNameError: false,
+    emailError: false,
+    passwordError: false,
+    phoneNumberError: false,
+  });
   const onChangeEvent = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
@@ -20,7 +26,7 @@ const LoopRender = () => {
       nameVal: "firstName",
       type: "text",
       placeholder: "First Name",
-      errorMessage: "Please enter valid name.",
+      errorMessage: "Please enter valid first name.",
       label: "FirstName",
     },
     {
@@ -28,7 +34,7 @@ const LoopRender = () => {
       nameVal: "lastName",
       type: "text",
       placeholder: "Last Name",
-      errorMessage: "Please enter valid name.",
+      errorMessage: "Please enter valid last name.",
       label: "LastName",
     },
     {
@@ -36,7 +42,7 @@ const LoopRender = () => {
       nameVal: "email",
       type: "text",
       placeholder: "Email",
-      errorMessage: "Please enter valid name.",
+      errorMessage: "Please enter valid email.",
       label: "Email",
     },
     {
@@ -44,44 +50,84 @@ const LoopRender = () => {
       nameVal: "password",
       type: "password",
       placeholder: "Password",
-      errorMessage: "Please enter valid name.",
+      errorMessage: "Please enter valid password.",
       label: "Password",
     },
     {
       id: 5,
       nameVal: "phoneNumber",
-      type: "number",
+      type: "text",
       placeholder: "Phone Number",
-      errorMessage: "Please enter valid name.",
+      errorMessage: "Please enter valid phone number.",
       label: "PhoneNumber",
     },
   ];
-  const validate = (e) => {
-        if (!e.target.value) {
-          console.log("inside Case IF part", e.target.value)
+  const validate =(id)=> (e) => {
+    // if (!e.target.value) {
+    //   setIsError((isError = true));
+ 
+    // } else {
+    //   setIsError(isError = false);
+    //   console.log(id);
+    // }
+    switch (id) {
+      case 0: {
+        if (!e.target.value || e.target.value.length>2) {
+          setIsError({...isError,firstNameError:true});
         } else {
-          console.log("inside Case ElsePart", e.target.value)
-       }
+          setIsError({...isError,firstNameError:false}) 
+        }
+        break;
       }
+      case 1: {
+        if (!e.target.value || e.target.value.length > 2) {
+          setIsError({...isError,lastNameError:true});
+        } else {
+          setIsError({...isError,lastNameError:false}) 
+        }
+        break;
+      }
+      case 2: {
+        if (e.target.value === "" ||
+        !e.target.value.includes("@") ||
+        !e.target.value.includes(".") ||
+        e.target.value.startsWith("@") ||
+        e.target.value.slice(e.target.value.lastIndexOf(".") + 1).length < 2) {
+          setIsError({...isError,emailError:true});
+        } else {
+          setIsError({...isError,emailError:false}) 
+        }
+        break;
+      }
+      case 3: {
+        if (!e.target.value || e.target.value.length < 6) {
+          setIsError({...isError,passwordError:true});
+        } else {
+          setIsError({...isError,passwordError:false}) 
+        }
+        break;
+      }
+      case 4: {
+        if (e.target.value === "" ||
+        e.target.value.length !== 10 ||
+        e.target.value[0] < 6) {
+          setIsError({...isError,phoneNumberError:true});
+        } else {
+          setIsError({...isError,phoneNumberError:false}) 
+        }
+        break;
+      }
+      default: {
+        return {...isError}
+        }
+    }
+  };
 
   return (
     <div className="signup_wrapper">
       <form>
         <h1>SignUp</h1>
-        <div className="form_Input">
-            <label htmlFor="firstName">firstName</label>
-            <input
-              placeholder="firstName"
-            type="text"
-            vlaue="firstName"
-              autoComplete="off"
-              onChange={onChangeEvent}
-              onFocus={validate }
-            />
-            <p className="errorMsg">"Error"</p>
-          </div>
-
-      {/* {inputValues.map((inputVal, index) => {
+      {inputValues.map((inputVal, index) => {
         return (
           <div key={index} className="form_Input">
             <label htmlFor={inputVal.label}>{inputVal.label}</label>
@@ -91,15 +137,18 @@ const LoopRender = () => {
               value={values[inputVal.name]}
               autoComplete="off"
               onChange={onChangeEvent}
-              onFocus={()=> validate(inputVal.name) }
+              onKeyUp={(e)=> validate(index)(e)}
             />
-            <p className="errorMsg">{inputVal.errorMessage}</p>
+            <p id="errorMsg">{isError[Object.keys(isError)[index]] ? inputVal.errorMessage:""}</p>
           </div>
         );
-      })} */}
-      <button>Submit</button>
-    </form>
+      })}
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
 export default LoopRender;
+
+
+//  console.log(Object.keys(isError)[index], isError[Object.keys(isError)[index]])
